07386529582329c6ef1f1719c2da8409480c573c
takaodaze <gin2798@gmail.com>
2025-02-20 17:09:35 +0900

    init

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import './style.css'
+import p5 from 'p5'
+
+
+
+export const start = () => {
+  const sketch = (p: p5) => {
+    p.setup = () => {
+      p.createCanvas(p.windowWidth, p.windowHeight)
+      p.background(220)
+      p.frameRate(30)
+    }
+
+    p.draw = () => {
+    }
+
+    p.mouseMoved = () => {
+      p.ellipse(p.mouseX, p.mouseY, 10, 10)
+    }
+  }
+  new p5(sketch)
+}
+
+start()
