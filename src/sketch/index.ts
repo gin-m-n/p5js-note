6d74d6ffac0969ef55cf042e5812f3d3e60b2d8b
takaodaze <gin2798@gmail.com>
2025-02-22 19:06:46 +0900

    new sketch

--- src/sketch/index.ts
+++ src/sketch/index.ts
@@ -7,6 +7,7 @@ import { start as start6 } from "./6";
 import { start as start7 } from "./7";
 import { start as start8 } from "./8";
 import { start as start9 } from "./noise-3d_1.ts";
+import { start as start10 } from "./noise-3d_2.ts";
 
 const sketchList = [
   { name: "1", start: start1 },
@@ -17,7 +18,8 @@ const sketchList = [
   { name: "6_noise-2", start: start6 },
   { name: "7_noise-3", start: start7 },
   { name: "8_noise-4", start: start8 },
-  { name: "9_3d-noise", start: start9 },
+  { name: "3d-noise_1", start: start9 },
+  { name: "3d-noise_2", start: start10 },
 ]
 
 export default sketchList
---
+++ src/sketch/noise-3d_2.ts
@@ -0,0 +1,61 @@
+import p5 from 'p5'
+
+export const start = (node: HTMLElement) => {
+  const sketch = (p: p5) => {
+    const BG_COLOR = 0;
+    const sideLength = 100
+    const step = 5
+
+    const drawPoint = (x: number, y: number, z: number, noise: number) => {
+      p.push()
+      p.translate(x - sideLength / 2, y - sideLength / 2, z - sideLength / 2)
+      const g = 255 * noise
+      p.fill(g, 10)
+      p.box(step, step, step)
+      p.pop()
+    }
+
+    p.setup = () => {
+      p.createCanvas(p.windowWidth, p.windowHeight, "webgl");
+      p.smooth();
+      p.background(BG_COLOR);
+      p.noStroke();
+      p.frameRate(20)
+    }
+
+    let xstart = p.random(10)
+    let ystart = p.random(10)
+    let zstart = p.random(10)
+
+    p.draw = () => {
+      const t = performance.now() / 1000
+      p.background(BG_COLOR);
+      xstart += 0.01
+      ystart += 0.01
+      zstart += 0.01
+
+      p.translate(0, 0, 600)
+      p.rotateX(t / 3)
+      p.rotateY(t / 3)
+
+      let znoise = zstart;
+      for (let z = 0; z < sideLength; z += step) {
+        znoise += 0.1
+        let ynoise = ystart;
+        for (let y = 0; y <= sideLength; y += step) {
+          ynoise += 0.1
+          let xnoise = xstart;
+          for (let x = 0; x <= sideLength; x += step) {
+            xnoise += 0.1
+            drawPoint(x, y, z, p.noise(xnoise, ynoise, znoise))
+          }
+        }
+      }
+    }
+  }
+
+  new p5(sketch, node)
+}
+
+
+
