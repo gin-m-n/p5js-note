ed24ebd8153d040865c358d8848eb3e10bc4a8ee
takaodaze <gin2798@gmail.com>
2025-02-22 23:52:10 +0900

    update sketch

--- src/sketch/index.ts
+++ src/sketch/index.ts
@@ -9,7 +9,7 @@ import { start as start8 } from "./part1-2/8";
 import { start as start9 } from "./part1-2/noise-3d_1";
 import { start as start10 } from "./part1-2/noise-3d_2";
 import { start as start11 } from "./part1-2/my-sphere";
-import { start as start12 } from "./part3/moveCircles";
+import { start as start12 } from "./part3/circle-emergence";
 
 const sketchList = [
   { name: "1", start: start1 },
@@ -23,7 +23,7 @@ const sketchList = [
   { name: "3d-noise_1", start: start9 },
   { name: "3d-noise_2", start: start10 },
   { name: "my-sphere", start: start11 },
-  { name: "moveCircles", start: start12 },
+  { name: "circle-emergence", start: start12 },
 ]
 
 export default sketchList
--- src/sketch/part3/circle-emergence.ts
+++ src/sketch/part3/circle-emergence.ts
@@ -1,5 +1,7 @@
 import p5, { Color } from 'p5'
+import GUI from 'lil-gui';
 
+const gui = new GUI();
 
 export const start = (node: HTMLElement) => {
   const sketch = (p: p5) => {
@@ -16,12 +18,13 @@ export const start = (node: HTMLElement) => {
       constructor() {
         this.x = p.random(p.width)
         this.y = p.random(p.height)
-        this.radius = p.random(90) + 10
+        this.radius = p.random(120) + 10
         this.linecol = p.color(p.random(255), p.random(255), p.random(255), 150)
         this.alpha = p.random(255)
         this.fillColor = p.color(p.random(255), p.random(255), p.random(255), this.alpha)
-        this.xStep = (-0.5 + p.random(1)) * 5
-        this.yStep = (-0.5 + p.random(1)) * 5
+        const speed = 5
+        this.xStep = (-0.5 + p.random(1)) * speed
+        this.yStep = (-0.5 + p.random(1)) * speed
       }
 
       drawMe() {
@@ -45,39 +48,62 @@ export const start = (node: HTMLElement) => {
         if (this.y > p.height + this.radius) this.y = 0 - this.radius
         if (this.y < 0 - this.radius) this.y = p.height + this.radius
 
-        const isTouching = circleArr
-          .filter((other) => this !== other)
-          .some((other) => this.isTouchingOther(other))
+      }
 
-        if (isTouching) {
-          this.alpha--
-        } else {
-          this.alpha += 2
-        }
-        this.fillColor.setAlpha(this.alpha)
+      overlap(other: Circle) {
+        const d = p.dist(this.x, this.y, other.x, other.y)
+        return d - this.radius - other.radius
+
       }
 
       isTouchingOther(other: Circle) {
-        const d = p.dist(this.x, this.y, other.x, other.y)
-        const isTouching = d - this.radius - other.radius < 0
-        return isTouching
+        return this.overlap(other) < 0
       }
 
       changeColor() {
         this.fillColor = p.color(255, 0, 0)
       }
     }
+
+    function addCircle(circle: Circle) {
+      circleArr.push(circle)
+      param.numberOfOriginCircle = circleArr.length
+    }
+
+    function drawChildCircles() {
+      circleArr.forEach((c) => {
+        circleArr
+          .filter((other) => c !== other)
+          .filter(other => c.isTouchingOther(other))
+          .forEach((other) => {
+            const midx = (c.x + other.x) / 2
+            const midy = (c.y + other.y) / 2
+            const overlap = -c.overlap(other)
+            p.stroke(0, 100)
+            p.noFill()
+            p.ellipse(midx, midy, overlap, overlap)
+          })
+      })
+    }
+
+
+
     const circleArr: Circle[] = []
+    const param = {
+      isOriginCircleDrawn: true,
+      isChildCircleDrawn: false,
+      numberOfOriginCircle: 0
+    }
     const BG_COLOR = 255;
     p.setup = () => {
       p.createCanvas(p.windowWidth, p.windowHeight);
-      p.frameRate(30)
+      p.frameRate(60)
       p.smooth();
       p.background(BG_COLOR);
       p.strokeWeight(1);
 
       Array(10).fill(0).forEach(() => {
-        circleArr.push(new Circle())
+        addCircle(new Circle())
       })
     }
 
@@ -86,15 +112,24 @@ export const start = (node: HTMLElement) => {
 
       circleArr.forEach((c) => {
         c.updateMe()
-        c.drawMe()
+        if (param.isOriginCircleDrawn) c.drawMe()
+      })
+
+      if (param.isChildCircleDrawn) drawChildCircles()
+    }
+
+    p.mouseReleased = (e) => {
+      // @ts-ignore
+      if (e.target.tagName !== "CANVAS") return
+
+      Array(10).fill(0).forEach(() => {
+        addCircle(new Circle())
       })
     }
 
-    p.mouseReleased = () => {
-      Array(10).fill(0).forEach(() => {
-        circleArr.push(new Circle())
-      })
-    }
+    gui.add(param, 'isOriginCircleDrawn');
+    gui.add(param, "isChildCircleDrawn")
+    gui.add(param, "numberOfOriginCircle").listen().disable()
   }
 
   new p5(sketch, node)
